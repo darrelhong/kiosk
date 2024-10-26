f65dd4d0a0813c95f0d37b89d5188d44ebd7bcc1
darrelhong <darrel.hong@gmail.com>
2024-10-26 22:55:58 +0800

    add button and dropdown menu components and dark mode toggle

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,3 +1,5 @@
+import { DarkModeToggle } from "@/components/dark-mode-toggle";
+import { Button } from "@/components/ui/button";
 import { createLazyFileRoute, Link } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/")({
@@ -8,11 +10,13 @@ function Landing() {
   return (
     <>
       <header className="sticky top-0 z-50 backdrop-blur">
-        <div className="container mx-auto flex h-14 items-center px-4">
+        <div className="container mx-auto flex h-14 items-center gap-x-4 px-4">
           <a className="me-auto text-2xl font-bold sm:text-3xl">Kiosk App</a>
           <Link to="/login" className="">
             Login
           </Link>
+
+          <DarkModeToggle />
         </div>
       </header>
 
