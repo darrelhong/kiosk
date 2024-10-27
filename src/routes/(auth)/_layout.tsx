d103ab3903bd613529376b3e9099b5b0f8def763
darrelhong <darrel.hong@gmail.com>
2024-10-27 21:10:09 +0800

    update header layout

--- src/routes/(auth)/_layout.tsx
+++ src/routes/(auth)/_layout.tsx
@@ -8,9 +8,11 @@ export const Route = createFileRoute("/(auth)/_layout")({
 function PublicLayout() {
   return (
     <>
-      <header className="sticky top-0 z-50 backdrop-blur">
-        <div className="container mx-auto flex h-14 items-center gap-x-4 px-4">
-          <a className="me-auto text-2xl font-bold sm:text-3xl">Kiosk App</a>
+      <header className="sticky top-0 z-50 border-b backdrop-blur">
+        <div className="container mx-auto flex min-h-14 items-center gap-x-4 px-4">
+          <a className="me-auto py-2 text-2xl font-bold sm:text-3xl">
+            Kiosk App
+          </a>
           <Link to="/login" className="[&.active]:hidden">
             Login
           </Link>
